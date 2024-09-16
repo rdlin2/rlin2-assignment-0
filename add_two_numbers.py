f9332985ce90a7f95b395ed2e45d0d8c65eae43c
rdlin2 <rlin2@bu.edu>
2024-09-15 22:57:47 -0400

    Create add_two_numbers.py

---
+++ add_two_numbers.py
@@ -0,0 +1,7 @@
+def add_numbers(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    num1 = float(input("Enter the 1st number: "))
+    num2 = float(input("Enter the 2nd number: "))
+    print("The sum is:", add_numbers(num1, num2))
